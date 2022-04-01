f5e9a97e679661f633be7a320602a79ad407d750
DmitrySaa <102784998+DmitrySaa@users.noreply.github.com>
2022-04-01 10:57:08 +0400

    Create FirstPackage.java

---
+++ FirstPackage.java
@@ -0,0 +1,27 @@
+class SecondClass
+{
+    private int a;
+
+    private int b;
+
+    //SecondClass var = new SecondClass();
+
+    int Summ(int a, int b){
+        int res = a + b;
+        return res;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+    public int getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+    public int getA() {
+        return a;
+    }
+}
